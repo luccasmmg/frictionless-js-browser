18c21b27a70974d9a4cc52841363764afa9cebb7
Luccas Mateus <luccasmmg@gmail.com>
2023-01-02 14:26:31 -0300

    feat: remove crypto

--- src/main.js
+++ src/main.js
@@ -6,4 +6,3 @@ export { FileInline } from "./file-inline";
 export { Dataset } from "./dataset";
 export { csvParser } from "./parser/csv";
 export { xlsxParser } from "./parser/xlsx";
-export { computeHash } from "./file-base";
